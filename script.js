cf6a837d10d25f715a72c07e0b6ec212c34f59a0
krlresearchlab <krleconomics@gmail.com>
2026-01-08 14:23:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+}
+
+window.scrollTo(0, 0);
 document.addEventListener("DOMContentLoaded", () => {
     const splash = document.querySelector('.splash-text');
     const subtitle = document.querySelector('.subtitle');
